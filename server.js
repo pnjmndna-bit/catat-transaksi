9ec39c57e7a710ed7b50d3777c3cb23decab4155
pnjmndna-bit <pnjmndna@gmail.com>
2026-08-05 18:57:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -207,7 +207,7 @@ app.use((req,res)=>{
 // START SERVER
 // ================================
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,()=>{
 
